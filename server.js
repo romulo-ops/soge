8bbc198f1e9a55747e01250cf30cb78e43b394f6
romulo-ops <uuromulo@gmail.com>
2026-02-10 16:25:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+
 const express = require("express");
 const path = require("path");
 const mysql = require("mysql2/promise");
